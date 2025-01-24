a83ae6ed7dd41870417d4685943ff0b34b4b159d
Manuel <121738651+Maxybom@users.noreply.github.com>
2025-01-24 22:53:52 +0100

    Clone the ocorn-imgui repository

---
+++ TrEngine/vendor/Imgui/premake5.lua
@@ -0,0 +1,42 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "off"
+    warnings "off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",  
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+    }
+
+    includedirs
+    {
+        ".",
+        "%{IncludeDir.GLFW}",  -- Assicurati che il percorso sia corretto
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
