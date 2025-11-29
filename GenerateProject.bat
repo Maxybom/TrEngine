39da49debecd6e359e4452b2958f01ca13f53061
Manuel <121738651+Maxybom@users.noreply.github.com>
2025-11-29 03:57:35 +0100

    Text change

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Pulizia dei file e delle cartelle...
+echo Clean start
 
 REM Cancella file specifici
 del /Q /F "Sandbox\Sandbox.vcxproj"
@@ -8,14 +8,12 @@ del /Q /F "TrEngine\TrEngine.vcxproj"
 del /Q /F "TrEngine\TrEngine.vcxproj.user"
 del /Q /F "TrEngine\TrEngine.vcxproj.filters"
 
-REM Cancella una directory e tutto il suo contenuto
 rmdir /S /Q "bin"
 rmdir /S /Q "bin-int"
 
-echo Pulizia completata.
-echo Esecuzione del comando premake...
+echo Clean complete
+echo premake execution
 
-REM Esegui il comando premake
 call vendor\bin\premake\premake5.exe vs2022
 
 PAUSE
